b6f31ba38eecc084e7bc750c525d559d11458ca9
mi_张帅 <mi_shuai.zhang@mihoyo.com>
2022-07-14 22:54:59 +0800

    feat: 新增登陆页，封装svg图标组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import installElementPlus from './plugins/element'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import installElementPlus from './plugins/element';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+// 导入全局样式
+import './styles/index.scss';
+// 导入 svgIcon
+import installIcons from '@/icons';
 
-const app = createApp(App)
-installElementPlus(app)
-app.use(store).use(router).mount('#app')
+const app = createApp(App);
+
+installElementPlus(app); // element ui plus
+installIcons(app); // svg-icon
+
+// 全局注册图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.use(store).use(router).mount('#app');
